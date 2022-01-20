735a2a3a54334829ab235e0aeb3d6c03f81b8d0f
Roopa2615 <rupashree1011@gmail.com>
2022-01-20 11:54:51 +0530

    updated todoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,39 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todo';
+
+  filter: 'all' | "active" | 'done' = "all";
+
+  allItems= [
+    { description: "eat", done: true },
+    { description: "play", done: true },
+    { description: "laugh", done: false },
+    { description: "sleep", done: false }
+  ]
+     
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    } else {
+      return this.allItems.filter((item)=> this.filter === 'done' ? item.done : !item.done)
+    }
+  }
+
+  addItem(description: string) {
+    if (!description) return;
+  this.allItems.unshift({
+    description,
+    done : false
+  });
+  }
+
+  remove(item: any) {
+  this.allItems.splice(this.allItems.indexOf(item), 1);
 }
+
+
+}
+
